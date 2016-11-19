7f5b8f2d7701a7a101e5314e49ec036a03925ffa
vchapple17 <vchapple17@gmail.com>
2016-11-18 18:09:55 -0600

    base skeleton with handlebars, body-parser, 404, 500, home

---
+++ helloSessions.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+
+// Express
+app.set('port', 40444);
+app.use(express.static('public'));
+
+// Handlebars
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Session
+app.route('/')
+   // GET
+   .get(function (req, res) {
+      var context = {};
+      res.render('home', context);
+   })
+
+// 404 Default
+app.use(function(req, res) {
+  res.type('plain/text');
+  res.status('404');
+  res.render('404');
+});
+
+// 500
+app.use(function(err, req, res, next) {
+   res.type('plain/text');
+   res.status('500');
+   res.render('500');
+});
+app.listen(app.get('port'), function() {
+   console.log('Express started on http://localhost:' + app.get('port') + "; Press Ctrl-C to terminate");
+});
+
